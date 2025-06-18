2b9001d6960ce54315656efd4314443f5ff36935
noel319 <azeisl917@gmail.com>
2025-06-18 16:10:55 -0400

    init project

---
+++ shedule_api/apps/core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import uuid
+
+
+class BaseModel(models.Model):    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+    def soft_delete(self):        
+        self.is_active = False
+        self.save(update_fields=['is_active', 'updated_at'])
+
+    def restore(self):        
+        self.is_active = True
+        self.save(update_fields=['is_active', 'updated_at'])
